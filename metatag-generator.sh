b4b5ab3b4e549c66050869454846950a9a031bc4
Your Name <you@example.com>
2021-11-19 10:40:21 -0800

    frist commit

---
+++ metatag-generator.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+echo "Site Name"
+read SiteName
+
+echo "Site Title (Characters left: 56):"
+read SiteTitle
+
+echo "Site Description (Characters left: 147):"
+read SiteDescription
+
+echo "Site Keywords (Separate with commas):"
+read SiteKeywords
+
+echo "Author:"
+read Author
+
+echo "Contact (Email):"
+read ContactEmail
+
+echo "Search engines should revisit this page after:"
+read revisit
+
+echo "What is your site primary language?"
+read language
+
+echo "<meta name=\"title\" content=\"${SiteTitle}\">" >> ${SiteName}.txt
+echo "<meta name=\"description\" content=\"${SiteDescription}\">" >> ${SiteName}.txt
+echo "<meta name=\"keywords\" content=\"${SiteKeywords}\">" >> ${SiteName}.txt
+echo "<meta name=\"author\" content=\"${Author}\">" >> ${SiteName}.txt
+echo "<meta name=\"contact\" content=\"${ContactEmail}\">" >> ${SiteName}.txt
+echo "<meta name=\"revisit-after\" content=\"${revisit}\">" >> ${SiteName}.txt
+echo "<meta name=\"robots\" content=\"index, follow\">" >> ${SiteName}.txt
+echo "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">" >> ${SiteName}.txt
+echo "<meta name=\"language\" content=\"${language}\">" >> ${SiteName}.txt
